6e60211a4d531d51b6a3ddd5036b4d40fca4b10b
mrkgelo2 <markangelo.servando@wvsu.edu.ph>
2024-11-11 20:55:02 +0800

    bottom nav bar

---
+++ lib/feature/bot_nav_bar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  final int currentIndex;
+
+  const Navigation({super.key, required this.currentIndex});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.currentIndex;
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, '/dashboard');
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, '/search');
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, '/schedule');
+        break;
+      case 3:
+        Navigator.pushReplacementNamed(context, '/messages');
+        break;
+      case 4:
+        Navigator.pushReplacementNamed(context, '/profile');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: _onTabTapped,
+      currentIndex: currentIndex,
+      fixedColor: Colors.black,
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search_outlined),
+          label: "Search",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_month_outlined),
+          label: "Schedule",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message_outlined),
+          label: "Messages",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined),
+          label: "Me",
+        ),
+      ],
+    );
+  }
+}
