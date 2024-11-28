c4555fecb9039f3f6d3d8248069dfb63bf695439
mrkgelo2 <markangelo.servando@wvsu.edu.ph>
2024-11-28 20:38:02 +0800

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'feature/sign_up.dart';
+import 'feature/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login',
+      title: 'Sign in',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
